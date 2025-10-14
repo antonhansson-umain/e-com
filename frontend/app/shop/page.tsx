fbe2668fc786ccc7ca817a4749dc782584b988b7
Anton Hansson <anton.hansson@umain.com>
2025-10-14 09:27:51 +0200

    feat: query all albums

--- frontend/app/shop/page.tsx
+++ frontend/app/shop/page.tsx
@@ -1,3 +1,13 @@
-export default function page() {
-  return <div>Shop page</div>
+import {sanityFetch} from '@/sanity/lib/live'
+import {getAlbumsQuery} from '@/sanity/lib/queries'
+
+export default async function page() {
+  const {data: albums} = await sanityFetch({query: getAlbumsQuery, params: {}})
+  return (
+    <>
+      {albums.map((album) => (
+        <li key={album._id}>{album.title}</li>
+      ))}
+    </>
+  )
 }
