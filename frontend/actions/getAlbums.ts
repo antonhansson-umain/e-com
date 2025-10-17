ba07dab297d34b3b1791f69fc539806cbebeb848
Anton Hansson <anton.hansson@umain.com>
2025-10-17 14:47:06 +0200

    fix: make filters parameter optional in getAlbums

--- frontend/actions/getAlbums.ts
+++ frontend/actions/getAlbums.ts
@@ -4,7 +4,7 @@ import {sanityFetch} from '@/sanity/lib/live'
 import {getAlbumsQuery} from '@/sanity/lib/queries'
 import {Filters} from '@/types/Filters'
 
-export async function getAlbums(filters: Filters) {
+export async function getAlbums(filters?: Filters) {
   const genres = filters?.genres ?? null
   const countries = filters?.countries ?? null
   const {data: albums} = await sanityFetch({
