2c52d37873e83156645019e4a77f2ec2308f4603
Anton Hansson <anton.hansson@umain.com>
2025-10-17 11:12:08 +0200

    feat: add related albums section back to product details page

--- frontend/app/shop/[id]/page.tsx
+++ frontend/app/shop/[id]/page.tsx
@@ -1,21 +1,23 @@
 import {getAlbum} from '@/actions/getAlbum'
 import {getAlbums} from '@/actions/getAlbums'
 import SelectedAlbumsSection from '@/app/components/SelectedAlbumsSection'
+import {notFound} from 'next/navigation'
 
 export default async function page({params}: {params: Promise<{id: string}>}) {
   const {id} = await params
   const album = await getAlbum(id)
-  // const relatedAlbums = await getAlbums({genres: album?.genres ?? []})
+  if (!album) return notFound()
+  const relatedAlbums = await getAlbums({genres: album.genres})
   return (
     <div>
       <h1>{album?.title}</h1>
-      {/* <SelectedAlbumsSection
+      <SelectedAlbumsSection
         albums={relatedAlbums.slice(0, 2)}
         title="You may also like..."
         cta="Explore"
         ctaHref="/shop"
         description="If you like this, you'll love these."
-      /> */}
+      />
     </div>
   )
 }
