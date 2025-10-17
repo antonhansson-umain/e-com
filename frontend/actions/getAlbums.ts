2edc90184f281c455884e34d29e0be560040a787
Anton Hansson <anton.hansson@umain.com>
2025-10-17 11:13:50 +0200

    chore: remove decade filter for now

--- frontend/actions/getAlbums.ts
+++ frontend/actions/getAlbums.ts
@@ -6,16 +6,14 @@ import {getAlbumsQuery} from '@/sanity/lib/queries'
 type Filters = {
   genres?: string[]
   countries?: string[]
-  decades?: number[]
 }
 
 export async function getAlbums(filters?: Filters) {
   const genres = filters?.genres ?? null
   const countries = filters?.countries ?? null
-  const decades = filters?.decades ?? null
   const {data: albums} = await sanityFetch({
     query: getAlbumsQuery,
-    params: {genres, countries, decades},
+    params: {genres, countries},
   })
   console.log(albums)
   return albums
