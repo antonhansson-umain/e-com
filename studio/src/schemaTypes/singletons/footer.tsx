1e2112ecad6c33b1c0aed2399a64ac32ba9b7e39
Anton Hansson <anton.hansson@umain.com>
2025-10-23 10:51:39 +0200

    refactor: use linkGroup object in footer schema type

--- studio/src/schemaTypes/singletons/footer.tsx
+++ studio/src/schemaTypes/singletons/footer.tsx
@@ -18,50 +18,7 @@ export const footer = defineType({
       type: 'array',
       name: 'linkGroups',
       title: 'Link Groups',
-      of: [
-        {
-          type: 'object',
-          title: 'Link Group',
-          name: 'linkGroup',
-          fields: [
-            {
-              type: 'string',
-              title: 'Group Title',
-              name: 'linkGroupTitle',
-              validation: (rule) => rule.required(),
-            },
-            {
-              type: 'array',
-              title: 'Links',
-              name: 'links',
-              of: [
-                {
-                  type: 'object',
-                  name: 'link',
-                  title: 'Link',
-                  fields: [
-                    {
-                      type: 'string',
-                      name: 'linkLabel',
-                      description: 'The label that should show instead of the actual link itself.',
-                      title: 'Link Label',
-                      validation: (rule) => rule.required(),
-                    },
-                    {
-                      type: 'string',
-                      name: 'linkPath',
-                      title: 'Link Path',
-                      description:
-                        "If it's a link to a page on this website, only use slug. If it's a link to an external website: use the full URL.",
-                      validation: (rule) => rule.required(),
-                    },
-                  ],
-                },
-              ],
-            },
-          ],
-        },
-      ],
+      of: [{type: 'linkGroup'}],
     }),
   ],
   preview: {
